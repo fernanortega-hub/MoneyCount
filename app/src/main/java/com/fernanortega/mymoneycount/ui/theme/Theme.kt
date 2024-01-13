83989f96ab10daf2e36566838b0af4f2b17b21c2
Fernando Ortega <00191820@uca.edu.sv>
2024-01-12 18:17:41 -0600

    Add surfaceTint color

--- app/src/main/java/com/fernanortega/mymoneycount/ui/theme/Theme.kt
+++ app/src/main/java/com/fernanortega/mymoneycount/ui/theme/Theme.kt
@@ -51,6 +51,7 @@ private val lightScheme = lightColorScheme(
     surfaceContainer = surfaceContainerLight,
     surfaceContainerHigh = surfaceContainerHighLight,
     surfaceContainerHighest = surfaceContainerHighestLight,
+    surfaceTint = primaryLight
 )
 
 private val darkScheme = darkColorScheme(
@@ -89,6 +90,7 @@ private val darkScheme = darkColorScheme(
     surfaceContainer = surfaceContainerDark,
     surfaceContainerHigh = surfaceContainerHighDark,
     surfaceContainerHighest = surfaceContainerHighestDark,
+    surfaceTint = primaryDark
 )
 
 @Composable
